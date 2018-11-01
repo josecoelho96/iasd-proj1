a27cb6fa9b1b35947c44cf0bc3455a4b895da7e5
José Coelho <josecoelho96@gmail.com>
2018-11-01 19:50:41 +0000

    Update simulator

--- play.py
+++ play.py
@@ -6,10 +6,12 @@ print("Welcome! Starting game...")
 
 # file_path = "maps/statement-map.map"
 # file_path = "maps/custom-map1.map"
-file_path = "maps/eye.map"
+# file_path = "maps/eye.map"
 # file_path = "maps/100.map"
 # file_path = "maps/1000.map"
 # file_path = "maps/custom-map2.map"
+file_path = "maps/custom-map3.map"
+
 
 
 game = go.Game()
@@ -20,3 +22,4 @@ with open(file_path) as f:
 print("Board loaded.")
 game.display(game.state)
 print("Valid moves: {}".format(game.actions(game.state)))
+print("Is terminal?: {}".format(game.terminal_test(game.state)))
