596502a11175f50ee0508858d078cccc78d196cf
José Coelho <josecoelho96@gmail.com>
2018-11-04 17:35:55 +0000

    Update simulator

--- play.py
+++ play.py
@@ -2,7 +2,7 @@
 
 import go
 import games
-
+import sys
 
 def main():
     print("Welcome! Starting game...")
@@ -19,22 +19,49 @@ def main():
     # file_path = "maps/custom-map5.map"
     # file_path = "maps/custom-map6.map"
     # file_path = "maps/custom-map7.map"
-    # file_path = "maps/tests/3-3.map"
+    file_path = "maps/tests/3-3.map"
     # file_path = "maps/tests/4-1.map"
     # file_path = "maps/tests/5-1.map"
     # file_path = "maps/tests/5-2.map"
     # file_path = "maps/tests/5-3.map"
     # file_path = "maps/tests/5-4.map"
     # file_path = "maps/tests/5-5.map"
-    file_path = "maps/tests/5-6.map"
+    # file_path = "maps/tests/5-6.map"
 
 
-    with open(file_path) as f:
+    with open(sys.argv[1]) as f:
+    # with open(file_path) as f:
         state = game.load_board(f)
 
     print("Board loaded.")
-    print("alpha_beta_cutoff_search(s,g) = {}".format(games.alphabeta_cutoff_search(state, game)))
-    # print("alpha_beta_cutoff_search(s,g,d=3) = {}".format(games.alphabeta_cutoff_search(state, game, d=3)))
+
+
+    ########################### TEST 2 ###########################
+    ########################### TEST 2 ###########################
+
+    ########################### TEST 3 ###########################
+    # print("Terminal test = {}".format(game.terminal_test(state)))
+    # # new_state = game.result(state, (1, 1, 5)) # 3.1
+    # # new_state = game.result(state, (2, 1, 2)) # 3.2
+    # new_state = game.result(state, (2, 5, 4)) # 3.3
+    # print("Terminal test = {}".format(game.terminal_test(new_state)))
+    # print("utility(s, 1)={}".format(game.utility(new_state, 1)))
+    # print("utility(s, 2)={}".format(game.utility(new_state, 2)))
+    ########################### TEST 3 ###########################
+
+    ########################### TEST 4 ###########################
+    # print("actions(s) = {}".format(game.actions(state)))
+    ########################### TEST 4 ###########################
+
+    ########################### TEST 5 ###########################
+    # if len(sys.argv) == 2:
+    #     print("alpha_beta_cutoff_search(s,g) = {}".format(games.alphabeta_cutoff_search(state, game)))
+    # else:
+    #     print("alpha_beta_cutoff_search(s,g,d={}) = {}".format(sys.argv[2], games.alphabeta_cutoff_search(state, game, d=int(sys.argv[2]))))
+    ########################### TEST 5 ###########################
+
+
+
 
 
 if __name__ == "__main__":
